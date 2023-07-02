91230f6e979205d63d995db7281e30fbd6a1b301
Jorge Melgarejo <melgarejo.colarte@gmail.com>
2023-07-01 20:12:58 -0400

    final

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,13 @@ const Footer = () => {
     return (
         <>
             <p className="socials">
-                <a href="https://github.com/joorgelm/jeto" rel="noreferrer" target="_blank"><VscGithub size={25}/></a>
+                <a href="https://github.com/joorgelm/jeto" title="back" rel="noreferrer" target="_blank">
+                    <VscGithub size={25}/>
+                    <em>back</em>
+                </a>
+                <a href="https://github.com/joorgelm/jeto-app" rel="noreferrer" target="_blank"><VscGithub size={25}/>
+                    <em>front</em>
+                </a>
                 <a href="https://www.linkedin.com/in/jlmelgarejo/" rel="noreferrer" target="_blank"><SiLinkedin
                     size={25}/>
                 </a>
